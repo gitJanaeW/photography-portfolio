9bf38fb8cf6e283cd5de4b73e4c44c36d0a83e2d
janae.wel <janae.wel@gmail.com>
2022-10-02 15:51:19 -0400

    react app created

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// ReactDOM library is rendering the App component at the root element in the HTML
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+reportWebVitals();
