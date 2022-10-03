d2236fc983ae0902b34d5539e9442022dea44a6c
janae.wel <janae.wel@gmail.com>
2022-10-03 18:20:05 -0400

    added tests for Nav and About

--- client/src/components/Nav/__tests__/index.test.js
+++ client/src/components/Nav/__tests__/index.test.js
@@ -4,20 +4,38 @@ import React from 'react';
 import {render, cleanup} from '@testing-library/react';
 // offers access to custom matchers that are used to test DOM elements
 import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import Nav from '..';
 
 // after each test, cleanup data in simulated DOM (refresh the sim DOM)
 afterEach(cleanup);
 
-describe('About component', () => {
+describe('Nav component', () => {
     // make sure about component renders
     it('renders', () => {
-        render(<About/>);
+        render(<Nav/>);
     });
-    // ???
-    it('matches snapshot DOM node structure', () => {
+    // tests snapshot
+    it('matches snapshot', () => {
         // asFragment: eturns a snapshot of the About component
-        const {asFragment} = render(<About/>);
+        const {asFragment} = render(<Nav/>);
         expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        // arrange the assert
+        const {getByTestId} = render(<Nav/>);
+        // assert
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
     });
 });
