79ca66f00ceac1637c437b1237fdb06d6d3eaec7
janae.wel <janae.wel@gmail.com>
2022-10-05 15:56:28 -0400

    added errorMessags for invalid email

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {validateEmail} from '../../utils/helpers';
 
 function Contact() {
+    const [formState, setFormState] = useState();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if(isValid) {
+                setErrorMessage('Your email is invalid');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+        }
+        if(!errorMessage) {
+            // the spread operator ensures we have access to formState as a whole while also singling out each attribute's
+            // value.
+            // [e.target.name] refers to the name attribute in the preceeding JSX, meaning when the name, email or message
+            // changes, we'll update the target (by its attribute name) with its new value
+            setFormState({...formState, [e.target.name]: e.target.value});
+        }
+        console.log('errorMessage:', errorMessage);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    console.log(formState);
     return (
         <section>
             <h1>Contact Me</h1> 
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label ftmlFor='name'>Name:</label>
-                    <input type='text' name='name'/>
+                    <input type='text' name='name' onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email'/>
+                    <input type='email' name='email' onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' row5='5'/>
+                    <textarea name='message' row5='5' onChange={handleChange}/>
                 </div>
                 <button type='submit'>submit</button> 
             </form>
@@ -24,4 +59,4 @@ function Contact() {
     )
 }
 
-rexport default Contact;
+export default Contact;
